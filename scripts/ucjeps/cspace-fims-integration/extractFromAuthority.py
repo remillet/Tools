a3c64055eb7405a4a0457a924ca5ccd8b46e2ad3
John Lowe <jblowe@berkeley.edu>
2017-11-16 12:25:31 -0800

    UCJEPS-562: minor revisions to extractFromAuthority.py: notes duplicates

--- scripts/ucjeps/cspace-fims-integration/extractFromAuthority.py
+++ scripts/ucjeps/cspace-fims-integration/extractFromAuthority.py
@@ -4,9 +4,11 @@ import sys, csv, codecs
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-def extractTag(xml, tag):
+
+def extract_tag(xml, tag):
     element = xml.find('.//%s' % tag)
     return element.text
+
 
 xData = []
 try:
@@ -16,15 +18,15 @@ try:
     for i in items:
         csid = i.find('.//csid')
         csid = csid.text
-        termDisplayName = extractTag(i, 'termDisplayName')
-        refName = extractTag(i, 'refName')
-        updated_at = extractTag(i, 'updatedAt')
-        xData.append([csid, termDisplayName,refName,updated_at])
+        termDisplayName = extract_tag(i, 'termDisplayName')
+        refName = extract_tag(i, 'refName')
+        updated_at = extract_tag(i, 'updatedAt')
+        xData.append([csid, termDisplayName, refName, updated_at])
+    del items, root, cspaceXML
 except:
     for row in csv.reader(codecs.open(sys.argv[1], 'r', "utf-8"), delimiter='\t'):
         # csid   displayname     refname noauthorname    majorgroup
         xData.append([row[0], row[1], row[2], 'n.d.'])
-
 
 cspaceCSV = csv.writer(codecs.open(sys.argv[2], 'w', "utf-8"), delimiter='\t')
 entities = {}
@@ -40,13 +42,18 @@ if len(sys.argv) >= 4:
 else:
     check_names = False
 
-for sequence_number,i in enumerate(xData):
-    [ csid,termDisplayName,refName,updated_at ] = i
+name_dict = {}
+for sequence_number, i in enumerate(xData):
+    [csid, termDisplayName, refName, updated_at] = i
     # if we were given a specific list of names, only write those ones out
     if check_names:
-        if not termDisplayName in names2check:
+        if termDisplayName not in names2check:
             continue
     else:
         pass
 
+    if termDisplayName in name_dict:
+        print '%s (%s) already seen as %s' % (termDisplayName, refName, name_dict[termDisplayName])
+    else:
+        name_dict[termDisplayName] = refName
     cspaceCSV.writerow([sequence_number, csid, termDisplayName, refName, updated_at])

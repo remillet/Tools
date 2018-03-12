0e6050e7fff6f566f3033882166ad8dd557a690c
John Lowe <jblowe@berkeley.edu>
2018-03-12 10:48:47 -0700

    DJAN-119: refactor some of the Solr utilities in prep for a version upgrade

---
+++ datasources/ucb/multicore/make_schema.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# mostly untested!
+set -e
+if [ $# -lt 1 ];
+then
+  echo 1>&2 "Make solr7 managed-schema from solr4 schema.xml, based on given managed-schema-template"
+  echo 1>&2 ""
+  echo 1>&2 "call with one argument, the path to the Tool repo:"
+  echo 1>&2 "$0 fullpathtotoolsdir"
+  echo 1>&2 ""
+  echo 1>&2 "e.g."
+  echo 1>&2 "$0  ~/Tools"
+  echo 1>&2 ""
+  echo 1>&2 "(makes new files in the repo...)"
+  echo 1>&2 ""
+fi
+
+TOOLS=$1
+if [ ! -d $TOOLS ];
+then
+   echo "Tools directory $TOOLS not found. Please clone from GitHub and provide it as the first argument."
+   exit 1
+fi
+cd ${TOOLS}
+for t in bampfa botgarden ucjeps pahma cinefiles
+do
+  for type in public internal media
+    do
+      cp datasources/ucb/multicore/solrconfig.v5.xml datasources/ucb/multicore/${t}.${type}.solrconfig.xml
+      cp datasources/ucb/multicore/managed-schema-template temp.xml
+      grep '<copyField' datasources/ucb/multicore/${t}.${type}.schema.xml | grep -v '<\!--' | grep -v 'dest="text"/>' > temp.txt
+      perl -i -pe 's/^ //' temp.txt
+      perl -pe 's/COPYFIELDS/`cat temp.txt`/ge' -i temp.xml
+      perl -i -pe "s/\"example\"/\"${t}-${type}\"/" temp.xml
+      xmllint --format temp.xml > datasources/ucb/multicore/${t}.${type}.managed-schema
+      echo "${t}.${type}"
+    done
+done
+rm temp.txt temp.xml
--- datasources/utilities/curl4solr.sh
+++ datasources/utilities/curl4solr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# fetches all the publicly available solr4 datasources
+# fetches all the *publicly* available solr4 datasources
 #
 # NB: only work with current UCB setup.
 #     extracts are "manually curated" on Prod, and may not be the latest.
@@ -12,7 +12,7 @@ curl -O https://webapps.cspace.berkeley.edu/4solr.bampfa.public.csv.gz
 curl -O https://webapps.cspace.berkeley.edu/4solr.botgarden.propagations.csv.gz
 curl -O https://webapps.cspace.berkeley.edu/4solr.botgarden.public.csv.gz
 curl -O https://webapps.cspace.berkeley.edu/4solr.pahma.locations.csv.gz
-curl -O https://webapps.cspace.berkeley.edu/4solr.pahma.media.csv.gz
+curl -O https://webapps.cspace.berkeley.edu/4solr.pahma.allmedia.csv.gz
 curl -O https://webapps.cspace.berkeley.edu/4solr.pahma.osteology.csv.gz
 curl -O https://webapps.cspace.berkeley.edu/4solr.pahma.public.csv.gz
 curl -O https://webapps.cspace.berkeley.edu/4solr.ucjeps.media.csv.gz

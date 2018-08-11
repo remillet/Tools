741133168b846291d6b48f232ce486c347b4b1fc
John Lowe <jblowe@berkeley.edu>
2018-08-11 11:07:00 -0700

    UCJEPS-678: refactor authority extract scripts to use paging

---
+++ devops/helpers/extract_authorities.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+CREDS="xxx@ucjeps.cspace.berkeley.edu:xxx"
+ 
+function extract()
+{
+   curl -u "${CREDS}" -o tmp.xml "https://ucjeps.cspace.berkeley.edu/cspace-services/$1/items?pgSz=50000&wf_deleted=false"
+   xmllint --format tmp.xml > $2.xml
+   rm tmp.xml
+}
+
+extract orgauthorities/225e44ef-7f3d-4660-a4d6 nomenclature
+extract orgauthorities/751023ec-d953-45f9-a0a8 determination
+extract orgauthorities/a71f4ab6-221a-4202-bf75 institution
+extract orgauthorities/dcba2506-20fd-438b-9adc typeassertion
+# extract orgauthorities/f53284f1-0462-4326-92e7 organizationtest
+extract orgauthorities/6d89bda7-867a-4b97-b22f organization
+extract personauthorities/492326d1-efb1-4d2b-96d9 person
+# extract taxonomyauthority/87036424-e55f-4e39-bd12 taxonomyauthority
---
+++ devops/helpers/xmlcombine.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+# respectfully and with appreciation pillaged from:
+#
+# https://stackoverflow.com/questions/9004135/merge-multiple-xml-files-from-command-line
+#
+# a couple minor modifications for our purposes made.
+
+import sys
+from xml.etree import ElementTree
+
+def run(files):
+    first = None
+    for filename in files:
+        data = ElementTree.parse(filename).getroot()
+        if first is None:
+            first = data
+        else:
+            first.append(data)
+    if first is not None:
+        print ElementTree.tostring(first)
+
+if __name__ == "__main__":
+    run(sys.argv[1:])

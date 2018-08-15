2612e5472e86bcf699365ca6a3b71a1c9eee08b4
John Lowe <jblowe@berkeley.edu>
2018-08-15 13:36:21 -0700

    UCJEPS-678: refactor authority extract scripts to use paging

--- devops/helpers/extract_authorities.sh
+++ devops/helpers/extract_authorities.sh
@@ -1,19 +1,58 @@
 #!/usr/bin/env bash
 
+# make cURLs to get pages of <list-item> elements and merge them into one XML file
+# (a way to get all the records in a cspace procedure / authority / etc)
+#
+# NB: v5.0 has a max result size of 2500
+
+# invoke as:
+# ./extract.sh <cspace-object> <xmloutputfile>
+#
+# e.g.
+#
+# ./extract.sh orgauthorities/dcba2506-20fd-438b-9adc typeassertion.xml
+
+# set these to appropriate cspace login and password
 CREDS="xxx@ucjeps.cspace.berkeley.edu:xxx"
- 
+SERVER="https://ucjeps.cspace.berkeley.edu"
+# number of records get get
+PAGESIZE=1000
+# maximum number of cURLs to issue
+MAXCURLS=1000
+# ergo, maximum number of records that can be retrieved with these settings is
+# MAXCURLS * PAGESIZE = 1,000,000
+
+
 function extract()
 {
-   curl -u "${CREDS}" -o tmp.xml "https://ucjeps.cspace.berkeley.edu/cspace-services/$1/items?pgSz=50000&wf_deleted=false"
-   xmllint --format tmp.xml > $2.xml
-   rm tmp.xml
+   cat <<HERE > /tmp/new1.xml
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<list-wrapper>
+</list-wrapper>
+HERE
+
+   PAGENUM=0
+   while [ $MAXCURLS -gt 0 ]; do
+       curl -u "${CREDS}" -o /tmp/tmp.xml "${SERVER}/cspace-services/$1/items?pgSz=${PAGESIZE}&pgNum=${PAGENUM}&wf_deleted=false"
+
+
+       if grep -q "<itemsInPage>0</itemsInPage>" /tmp/tmp.xml
+       then
+            break
+       fi
+
+       python xmlcombine.py /tmp/new1.xml /tmp/tmp.xml > /tmp/new2.xml
+       mv /tmp/new2.xml /tmp/new1.xml
+
+       MAXCURLS=`expr $MAXCURLS - 1`
+       PAGENUM=`expr $PAGENUM + 1`
+       echo "Page ${PAGENUM}"
+
+    done
+
+   xmllint --format /tmp/new1.xml > $2
+   rm /tmp/tmp.xml /tmp/new1.xml
 }
 
-extract orgauthorities/225e44ef-7f3d-4660-a4d6 nomenclature
-extract orgauthorities/751023ec-d953-45f9-a0a8 determination
-extract orgauthorities/a71f4ab6-221a-4202-bf75 institution
-extract orgauthorities/dcba2506-20fd-438b-9adc typeassertion
-# extract orgauthorities/f53284f1-0462-4326-92e7 organizationtest
-extract orgauthorities/6d89bda7-867a-4b97-b22f organization
-extract personauthorities/492326d1-efb1-4d2b-96d9 person
-# extract taxonomyauthority/87036424-e55f-4e39-bd12 taxonomyauthority
+
+extract $1 $2
--- devops/helpers/extract_authorities_nightly.sh
+++ devops/helpers/extract_authorities_nightly.sh
@@ -1,49 +1,14 @@
 #!/usr/bin/env bash
 
-CREDS="xxx@ucjeps.cspace.berkeley.edu:xxx"
-PAGESIZE=2000
-STILLTODO=1
-
-
-function extract()
-{
-   cat <<HERE > new1.xml
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<ns2:abstract-common-list xmlns:ns2="http://collectionspace.org/services/jaxb">
-</ns2:abstract-common-list>
-HERE
-
-   while [ $STILLTODO -ne 0 ]; do
-       PAGENUM=0
-       curl -u "${CREDS}" -o tmp.xml "https://ucjeps.cspace.berkeley.edu/cspace-services/$1/items?pgSz=${PAGESIZE}&pgNum=${PAGENUM}&wf_deleted=false"
-
-
-       if [ `grep "<itemsInPage>0</itemsInPage>" tmp.xml` ]
-       then
-            break
-       fi
-
-       python combinexml.py new1.xml tmp.xml > new2.xml
-       mv new2.xml new1.xml
-
-       PAGENUM=`expr $PAGENUM + 1`
-       echo "Page ${PAGENUM}"
-
-    done
-
-   xmllint --format new2.xml > ~/extracts/ucjeps-authorities/$2.xml
-   rm tmp.xml new1.xml new2.xml
-}
-
-
-
-extract orgauthorities/225e44ef-7f3d-4660-a4d6 nomenclature
-extract orgauthorities/751023ec-d953-45f9-a0a8 determination
-extract orgauthorities/a71f4ab6-221a-4202-bf75 institution
-extract orgauthorities/dcba2506-20fd-438b-9adc typeassertion
-# extract orgauthorities/f53284f1-0462-4326-92e7 organizationtest
-extract orgauthorities/6d89bda7-867a-4b97-b22f organization
-extract personauthorities/492326d1-efb1-4d2b-96d9 person
-# extract taxonomyauthority/87036424-e55f-4e39-bd12 taxonomyauthority
+./extract_authorities.sh orgauthorities/225e44ef-7f3d-4660-a4d6 ~/extracts/ucjeps-authorities/nomenclature.xml
+./extract_authorities.sh orgauthorities/751023ec-d953-45f9-a0a8 ~/extracts/ucjeps-authorities/determination.xml
+./extract_authorities.sh orgauthorities/a71f4ab6-221a-4202-bf75 ~/extracts/ucjeps-authorities/institution.xml
+./extract_authorities.sh orgauthorities/dcba2506-20fd-438b-9adc ~/extracts/ucjeps-authorities/typeassertion.xml
+# ./extract_authorities.sh orgauthorities/f53284f1-0462-4326-92e7 ~/extracts/ucjeps-authorities/organizationtest.xml
+./extract_authorities.sh orgauthorities/6d89bda7-867a-4b97-b22f ~/extracts/ucjeps-authorities/organization.xml
+./extract_authorities.sh personauthorities/492326d1-efb1-4d2b-96d9 ~/extracts/ucjeps-authorities/person.xml
+# taxonomy is too big, and this data is already being extracted via SQL for the CCH project
+# ./extract_authorities.sh taxonomyauthority/87036424-e55f-4e39-bd12 ~/extracts/ucjeps-authorities/taxonomyauthority.xml
+# out with the old, in with the new...
 rm -f  ~/extracts/ucjeps-authorities/authorities.tgz
 tar -czf  ~/extracts/ucjeps-authorities/authorities.tgz ~/extracts/ucjeps-authorities

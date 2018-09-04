62a008bcf2cedda71f8922e1d190676045cc7de1
John Lowe <jblowe@berkeley.edu>
2018-09-04 15:51:42 -0700

    UCJEPS-667: 'eximup' contains script useful in uploading DWC-type .csv data into CSpace

---
+++ scripts/ucjeps/eximup/set-config-ucbg-prod.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+export CSPACEURL="https://botgarden.cspace.berkeley.edu"
+export CSPACEUSER="jblowe@berkeley.edu:Pumpkins99"
+echo
+echo ">>>>> Environment variables set:"
+echo CSPACEURL  $CSPACEURL
+echo CSPACEUSER $CSPACEUSER
+echo

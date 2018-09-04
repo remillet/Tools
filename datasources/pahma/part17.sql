2948be1337dd2f3bd9632721ac9c26d9a6693900
John Lowe <jblowe@berkeley.edu>
2018-09-04 15:53:14 -0700

    PAHMA-1651: repair 'culture or time period' tree values

--- datasources/pahma/part17.sql
+++ datasources/pahma/part17.sql
@@ -4,7 +4,7 @@ pc.velevation AS "objfcpelevation_s",
 utils.placename_hierarchy.place_hierarchy AS "objfcptree_ss"
 FROM collectionobjects_common cc
 JOIN collectionobjects_pahma_pahmafieldcollectionplacelist fcp ON (fcp.id=cc.id AND (fcp.pos=0 OR fcp.pos IS NULL))
-JOIN places_common pc ON (pc.shortidentifier=REGEXP_REPLACE(fcp.item, '^.*item:name\((.*)\)''.*', '\1'))
+JOIN places_common pc ON (pc.shortidentifier=REGEXP_REPLACE(fcp.item, '^.*item:name\((.*?)\)''.*', '\1'))
 JOIN hierarchy pcsid ON (pc.id=pcsid.id)
 LEFT OUTER JOIN utils.placename_hierarchy ON (pcsid.name=utils.placename_hierarchy.placecsid)
 LEFT OUTER JOIN hierarchy hp ON (hp.parentid=pc.id AND hp.primarytype='placeGeoRefGroup' AND (hp.pos=0 OR hp.pos IS NULL))

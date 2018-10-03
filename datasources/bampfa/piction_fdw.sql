56c6ed678991af9f90e8aa4221f28265e2964dd5
Lam Voong <lkv@berkeley.edu>
2018-10-03 14:41:05 -0700

    NOJIRA: correct parameter values for Prod

--- datasources/bampfa/piction_fdw.sql
+++ datasources/bampfa/piction_fdw.sql
@@ -9,13 +9,13 @@ create server bampfa_dev_server
 foreign data wrapper postgres_fdw
 options (host 'dba-postgres-dev-42', port '5114', dbname 'bampfa_domain_bampfa');
 
-create user mapping for piction server bampfa_dev_server
+create user mapping for piction server bampfa_prod_server
 options (user 'piction_ro', password 'secretpassword');
 
 -- Prod-02
-create server bampfa_dev_server
+create server bampfa_prod_server
 foreign data wrapper postgres_fdw
-options (host 'dba-postgres-dev-42', port '5313', dbname 'bampfa_domain_bampfa');
+options (host 'dba-postgres-prod-42', port '5313', dbname 'bampfa_domain_bampfa');
 
-create user mapping for piction server bampfa_dev_server
+create user mapping for piction server bampfa_prod_server
 options (user 'piction_ro', password 'secretpassword');

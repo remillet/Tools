b4204ae928b9d55755587d3368406aa385857ebe
Lam Voong <lkv@berkeley.edu>
2018-10-03 14:37:50 -0700

    NOJIRA: create piction_fdw.sql

---
+++ datasources/bampfa/piction_fdw.sql
@@ -0,0 +1,21 @@
+/*
+Source DB: bampfa_domain_bampfa
+Target DB: piction_transit
+DB User: piction_ro
+*/
+
+-- Dev-02
+create server bampfa_dev_server
+foreign data wrapper postgres_fdw
+options (host 'dba-postgres-dev-42', port '5114', dbname 'bampfa_domain_bampfa');
+
+create user mapping for piction server bampfa_dev_server
+options (user 'piction_ro', password 'secretpassword');
+
+-- Prod-02
+create server bampfa_dev_server
+foreign data wrapper postgres_fdw
+options (host 'dba-postgres-dev-42', port '5313', dbname 'bampfa_domain_bampfa');
+
+create user mapping for piction server bampfa_dev_server
+options (user 'piction_ro', password 'secretpassword');

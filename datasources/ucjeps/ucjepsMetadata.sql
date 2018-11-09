51b70f67cc635c79e448ab7873ede8c9d75ba619
John Lowe <jblowe@berkeley.edu>
2018-11-09 12:20:05 -0800

    UC-35: country, state, and country displaynames now extract from refnames

--- datasources/ucjeps/ucjepsMetadata.sql
+++ datasources/ucjeps/ucjepsMetadata.sql
@@ -39,10 +39,10 @@ select
             'yyyy-mm-dd')
         else null
     end as latecollectiondate_dt,
-    regexp_replace(lg.fieldlocverbatim,E'[\\t\\n\\r]+', ' ', 'g') as locality_s,
-    lg.fieldloccounty as collcounty_s,
-    lg.fieldlocstate as collstate_s,
-    lg.fieldloccountry as collcountry_s,
+    -- regexp_replace(lg.fieldlocverbatim,E'[\\t\\n\\r]+', ' ', 'g') as locality_s,
+    regexp_replace(lg.fieldloccounty, '^.*\)''(.*)''$', '\1') as collcounty_s,
+    regexp_replace(lg.fieldlocstate, '^.*\)''(.*)''$', '\1') as collstate_s,
+    regexp_replace(lg.fieldloccountry, '^.*\)''(.*)''$', '\1') as collcountry_s,
     lg.velevation as elevation_s,
     lg.minelevation as minelevation_s,
     lg.maxelevation as maxelevation_s,
@@ -59,8 +59,10 @@ select
     lg.coorduncertainty as coordinateuncertainty_f,
     lg.coorduncertaintyunit as coordinateuncertaintyunit_s,
     lg.localitynote as localitynote_s,
-    lg.localitysource as localitysource_s,
-    lg.localitysourcedetail as localitysourcedetail_s,
+    -- lg.localitysource as localitysource_s,
+    -- lg.localitysourcedetail as localitysourcedetail_s,
+    lg.georeferencesource as georeferencesource_s,
+    lg.georeferencesourceremarks as georeferencesourceremarks_s,
     cc.updatedat as updatedat_dt,
     case when conh.labelheader like 'urn:%' then getdispl(conh.labelheader)
         else conh.labelheader
@@ -135,14 +137,6 @@ select
     case when (fc.item is not null and fc.item <> '')
                 then regexp_replace(regexp_replace(fc.item, '^.*\)''(.*)''$', '\1'),E'[\\t\\n\\r]+', ' ', 'g')
     end as collectorverbatim_s,
-  array_to_string(array
-      (SELECT CASE WHEN (lg2.fieldlocverbatim IS NOT NULL AND lg2.fieldlocverbatim <>'' and lg2.fieldlocverbatim not like '%unknown%') THEN (getdispl(lg2.fieldlocverbatim)) ELSE '' END
-        from collectionobjects_common co5
-	      inner join hierarchy h5int on co5.id = h5int.id
-	      left outer join hierarchy hlg2 on (co5.id = hlg2.parentid and hlg2.pos > 0
-	      and hlg2.name = 'collectionobjects_naturalhistory:localityGroupList')
-	      left outer join localityGroup lg2 on (lg2.id = hlg2.id)
-        where h5int.name=h1.name order by hlg2.pos), '␥', '') as otherlocalities_ss,
   array_to_string(array
       (SELECT CASE WHEN (lg2.fieldlocverbatim IS NOT NULL AND lg2.fieldlocverbatim <>'' and lg2.fieldlocverbatim not like '%unknown%') THEN (getdispl(lg2.fieldlocverbatim)) ELSE '' END
         from collectionobjects_common co5

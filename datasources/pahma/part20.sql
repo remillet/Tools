95d42708c93d84e390183b9ff1d9bf4c16d2ccaf
John Lowe <jblowe@berkeley.edu>
2018-12-12 05:53:15 -0800

    PAHMA-1706: query and ETL changes to support 'deaccessioned'

---
+++ datasources/pahma/part20.sql
@@ -0,0 +1,14 @@
+SELECT cc.id, 'deaccessioned' AS deaccessioned_s,
+STRING_AGG(DISTINCT REGEXP_REPLACE(osl.item, '^.*\)''(.*)''$', '\1'),'␥') AS "status_ss"
+FROM collectionobjects_common cc
+LEFT OUTER JOIN collectionobjects_pahma_pahmaobjectstatuslist osl ON (cc.id=osl.id)
+JOIN misc m ON (m.id=cc.id AND m.lifecyclestate<>'deleted')
+WHERE osl.item = 'deaccessioned' 
+OR osl.item = 'transferred' 
+OR osl.item = 'repatriated' 
+OR osl.item = 'sold' 
+OR osl.item = 'exchanged' 
+OR osl.item = 'discarded' 
+OR osl.item = 'red-lined' 
+OR osl.item = 'destroyed' 
+GROUP BY cc.id;

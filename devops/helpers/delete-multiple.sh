03ec93eaa65cfcc32b525ae8a4005c62cced50b9
John Lowe <jblowe@berkeley.edu>
2018-12-26 09:19:07 -0800

    UJ-61: fix helper script used in testing

--- devops/helpers/delete-multiple.sh
+++ devops/helpers/delete-multiple.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+RUN_DIR=$(dirname $0)
 if [ "$CSPACEURL" == "" ] || [ "$CSPACEUSER" == "" ]; then
     echo "CSPACEURL and/or CSPACEUSER environment variables are not set. Did you edit set-config.sh and 'source set-config.sh'?"
     exit
@@ -12,7 +13,7 @@ fi
 
 if [ -r $2 ];
 then
-  for item in  `cut -f1 $2` ; do ./delete-single.sh $1 $item ; done 
+  for item in  `cut -f1 $2` ; do $RUN_DIR/delete-single.sh $1 $item ; done
 else
   echo "$2 -- list of csids not found."
 fi

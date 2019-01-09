75513d5ad534d32df9fec08d1a6c4af01eb57987
cesarvh <cesarv.h@berkeley.edu>
2019-01-08 16:44:44 -0800

    CF-14: Changed JOIN on media_cinefiles to LEFT OUTER JOIN

--- datasources/cinefiles/media_public.sql
+++ datasources/cinefiles/media_public.sql
@@ -15,7 +15,7 @@ mc.contributor
 
 FROM media_common mc
 
-JOIN media_cinefiles mb on (mb.id = mc.id)
+LEFT OUTER JOIN media_cinefiles mb on (mb.id = mc.id)
 JOIN misc ON (mc.id = misc.id AND misc.lifecyclestate <> 'deleted')
 LEFT OUTER JOIN hierarchy h1 ON (h1.id = mc.id)
 INNER JOIN relations_common r on (h1.name = r.objectcsid)
